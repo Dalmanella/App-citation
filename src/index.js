dc8a6d64fa83794d4550e383f3abc7e24d38549b
Muriel <h.muriel77@free.fr>
2022-02-19 14:11:55 +0100

    test encore...

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import Apropos from './pages/Apropos.js'
-import Dico from './pages/Dico.js'
-import Generateur from './pages/Generateur.js'
-import Navbar from './composants/Navbar.js'
+import Apropos from './pages/Apropos.js';
+import Dico from './pages/Dico.js';
+import Navbar from './composants/Navbar.js';
 import NotMatch from './pages/NotMatch';
+import Generateur from './pages/Generateur.js';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,15 +16,12 @@ ReactDOM.render(
       <Navbar/>
 
       <Routes>
-
         
         <Route path="/" element={<Generateur />}/>
         <Route path="/dico" element={<Dico />}/>
         <Route path="/apropos" element={<Apropos />}/>
         <Route path="*" element={<NotMatch />}/>
-
-        
-
+      
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
