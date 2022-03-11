4b989ec391a0ec76967882c165b7141f8f98be9a
Muriel <h.muriel77@free.fr>
2022-03-11 16:15:37 +0100

    correction orthographe

--- src/pages/Apropos.js
+++ src/pages/Apropos.js
@@ -13,7 +13,7 @@ const Apropos =() =>{
             <ul>
                 <li>L'appli contient un ensemble de citations Shadoks.</li>
                 <li>La page Dico vous permet de visualiser l'ensemble des citations enregistrées dans votre dictionnaire.</li>
-                <li>Vous pouvez ajoutez vos propres citations, atention un auteur et une citation vous seront demandés, l'ajout d'une image est facultatif. </li>
+                <li>Vous pouvez ajoutez vos propres citations, attention un auteur et une citation vous seront demandés, l'ajout d'une image est facultatif. </li>
                 <li>Vous pouvez modifier ou supprimer une citation que vous avez ajoutée.</li>
             </ul>
             <p>En revanche les shadoks resteront éternellement avec vous car les Shadoks pompaient, pompaient, pompaient...</p>
